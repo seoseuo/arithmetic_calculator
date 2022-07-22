2969ea84b1a8e4ef53d4455b9cd831fd5a0d8d79
seoseuo <seoseuo@naver.com>
2022-07-23 03:45:57 +0900

    0.1

---
+++ app/src/main/java/com/example/arithmetic_calculator/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.arithmetic_calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void addClick(View v)
+    {
+        EditText number1 = (EditText)findViewById(R.id.number1);
+        EditText number2 = (EditText)findViewById(R.id.number2);
+        TextView result = (TextView)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt(number1.getText().toString());
+        int n2 = Integer.parseInt(number2.getText().toString());
+
+        result.setText("결과는 "+Integer.toString((n1+n2)));
+    }
+
+    public void subClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText)findViewById(R.id.number2);
+        TextView result = (TextView)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt((number1.getText().toString()));
+        int n2 = Integer.parseInt((number2.getText().toString()));
+
+        result.setText("결과는 "+Integer.toString((n1-n2)));
+    }
+
+    public void mulClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText)findViewById(R.id.number2);
+        TextView result = (TextView)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt((number1.getText().toString()));
+        int n2 = Integer.parseInt((number2.getText().toString()));
+
+        result.setText("결과는 "+Integer.toString((n1*n2)));
+    }
+
+    public void divClick(View v) {
+        EditText number1 = (EditText) findViewById(R.id.number1);
+        EditText number2 = (EditText)findViewById(R.id.number2);
+        TextView result = (TextView)findViewById(R.id.result);
+
+        int n1 = Integer.parseInt((number1.getText().toString()));
+        int n2 = Integer.parseInt((number2.getText().toString()));
+
+        result.setText("몫은"+Integer.toString((n1/n2))+", 나머지는"+Integer.toString(n1%n2)+"입니다.");
+    }
+
+
+}
